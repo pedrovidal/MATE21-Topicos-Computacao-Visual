35d09f5aa5e3e68e146685e03720e89f2acd45b9
pedrovidal <peu_vs@yahoo.com.br>
2018-09-11 22:52:08 -0300

    Remove unnecessary code

--- proj.py
+++ proj.py
@@ -17,37 +17,35 @@ def load_data(path):
 	labels = np.array(labels)
 	return train, labels
 
-def teste(data, labels):
+def debug(data, labels):
 	print(len(data), len(labels))
 	for i in range(10):
-		cv2.imshow("teste", data[i])
+		cv2.imshow("debug", data[i])
 		print(labels[i])
 		cv2.waitKey(0)
 	
-def split_dataset(data, labels, train_per_val):
+def split_dataset(data, labels, train_percentage):
 	data_size = len(data)
 	permutation_index = np.random.permutation(data_size)
-	# print(permutation)
-	train_size = int(data_size * train_per_val / 100)
-	validation_size = data_size - train_size
-	print(train_size, validation_size)
+
+	train_size = int(data_size * train_percentage / 100)
 	
 	train_data = data[permutation_index[0:train_size]]
 	train_labels = labels[permutation_index[0:train_size]]
+
 	validation_data = data[permutation_index[train_size:data_size]]
 	validation_labels = labels[permutation_index[train_size:data_size]]
+
 	return train_data, train_labels, validation_data, validation_labels
 
 def main():
 	data, labels = load_data('./data_part1/train')
-	# teste(data, labels)
-	# print(len(data))
+	train_percentage = 80
 	train_data, train_labels, \
-		validation_data, validation_labels = split_dataset(data, labels, 80)
-	print("teste validation")
-	teste(validation_data, validation_labels)
-	print("teste train")
-	teste(train_data, train_labels)
+		validation_data, validation_labels = split_dataset(data, labels, train_percentage)
+
+	debug(train_data, train_labels)
+	debug(validation_data, validation_labels)
 
 if __name__ == "__main__":
 	np.random.seed(1)
