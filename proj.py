34f98731f65ebaf39c3c84eb061748d918d54aa4
Pedro Vidal <peu_vs@yahoo.com.br>
2018-09-14 14:54:25 -0300

    Implement gradient descent

--- proj.py
+++ proj.py
@@ -19,18 +19,19 @@ def initB():
 	return 0
 	
 def load_data(path):
-	train = []
+	data = []
 	labels = []
 
 	for dirs in sorted(os.listdir(path)):
 		for file_name in os.listdir(os.path.join(path, dirs)):
 			img_path = os.path.join(path, dirs, file_name)
-			train.append(cv2.imread(img_path, cv2.IMREAD_UNCHANGED))
-			labels.append(dirs)
+			data.append(cv2.imread(img_path, cv2.IMREAD_UNCHANGED))
+			labels.append(int(dirs))
 	
-	train = np.array(train)
+	data = np.array(data)
+	data /= 255
 	labels = np.array(labels)
-	return train, labels
+	return data, labels
 
 def shuffle(data, labels):
 	data_size = len(data)
@@ -59,50 +60,82 @@ def gradient_descent(w0, b0, batch_inputs, batch_labels, learning_rate):
 	# num_pixels = numero de pixels de cada imagem
 	num_pixels = batch_inputs[0].shape[0]
 
-	print(batch_size, num_pixels, len(w0), len(b0))
+	# print(batch_size, num_pixels, len(w0), len(b0))
 
 	grad_b = 0
-	grad_w = np.zeros(batch_size)
+	grad_w = np.zeros(num_pixels)
 
-
-	for i in range(batch_size):
-		y_i = batch_labels[i]
+	loss = 0
+	for x, y in zip(batch_inputs, batch_labels):
 		# derivada = (2.0 / batch_size) * (w0[0] * x_i0 + ... + w0[M-1] * x_iM-1 + b0 - y_i)
-		# derivada = (2.0 / batch_size) * soma
-	# 	soma = b0 - y_i
-	# 	for j in range(num_pixels):
-	# 		x_ij = batch_inputs[i, j]
-	# 		pre_calc += w0[j] * x_ij 
-	# 	grad_b += (2.0 / batch_size) * parte
-	# 	for j in range(num_pixels):
-	# 			grad_w[j] += (2.0 / batch_size) * x_ij * parte
+		# derivada = (2.0 / batch_size) * pre_calc
+		y_ = np.dot(w0, x) + b0 - y
+		loss += np.sum((y - y_)**2)
 
-	# b1 = b0 - (learning_rate * grad_b)
-	# w1 = w0 - (learning_rate * grad_w)
+		grad_b += 2.0 * y_
 
-	# return b1, w1
+		grad_w += 2.0 * x * y_
 
-def train(train_data, train_labels, validation_data, validation_labels, num_steps):
+		# grad_b += (2.0 / batch_size) * pre_calc
+		# for j in range(num_pixels):
+		# 	x_ij = batch_inputs[i, j]
+		# 	grad_w[j] += (2.0 / batch_size) * x_ij * pre_calc
+
+	loss /= batch_size
+	grad_b /= batch_size
+	grad_w /= batch_size
+
+	b1 = b0 - (learning_rate * grad_b)
+	w1 = w0 - (learning_rate * grad_w)
+
+	return b1, w1
+
+def validation(w, b, validation_data, validation_labels):
+	ac = 0.0
+	validation_size = len(validation_data)
+	for i in range(validation_size):
+		prediction = np.dot(validation_data[i], w) + b
+		if int(prediction + 0.5) == validation_labels[i]:
+			ac += 1
+	return ac / validation_size
+
+def train(train_data, train_labels, validation_data, validation_labels):
 	# transforma imagem em vetor de pixels
 	train_data = train_data.reshape(np.shape(train_data)[0], -1)
+	validation_data = validation_data.reshape(np.shape(validation_data)[0], -1)
 	
 	train_size = len(train_data)
+	validation_size = len(validation_data)
 	print(np.shape(train_data))
 	
 	num_pixels = train_data[0].shape[0]
 
-	w0 = initW(num_pixels)
-	b0 = initB()
+	w = initW(num_pixels)
+	b = initB()
 
 	batch_size = 5
-
-
-
-	batch_inputs = np.array(train_data[0:batch_size])
-	batch_labels = np.array(train_labels[0:batch_size])
+	num_steps = train_size / batch_size
+	learning_rate = 5e-3
 	
-	learning_rate = 0.5
-	gradient_descent(w0, b0, batch_inputs, batch_labels, learning_rate)
+	ini = 0
+	fim = batch_size - 1
+
+	for x in range(10):
+		print("Epoca", x)
+		ini = 0
+		fim = batch_size - 1
+		for i in range(num_steps):
+			print("Step", i)
+			batch_inputs = np.array(train_data[ini:fim])
+			batch_labels = np.array(train_labels[ini:fim])
+			
+			b, w = gradient_descent(w, b, batch_inputs, batch_labels, learning_rate)
+
+			ini += batch_size
+			fim += batch_size
+
+			ac = validation(w, b, validation_data, validation_labels)
+			print(ac)
 
 def main():
 	need_shuffle = True
@@ -121,8 +154,8 @@ def main():
 	else:
 		train_data = data
 		train_labels = labels
-		validation_data = []
-		validation_labels = []
+		validation_data = np.array()
+		validation_labels = np.array()
 
 	train(train_data, train_labels, validation_data, validation_labels)
 
